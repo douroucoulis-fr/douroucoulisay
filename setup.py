2014d9b74bbac6ca52e17e80c74e7f3fc43972e8
“Juan <douroucoulis.fr@gmail.com>
2024-08-31 10:26:15 +0200

    Functional version.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='douroucoulisay',
-    version='0.1',
+    version='0.1.1.2',
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'douroucoulisay=your_package.your_module:douroucoulisay',
+    'console_scripts': [
+        'douroucoulisay=douroucoulisay.douroucoulisay:main',  # main function should handle argparse and call douroucoulisay
         ],
     },
     install_requires=[],
@@ -16,7 +16,7 @@ setup(
     },
     author='Aotus Parisinus',
     author_email='douroucoulis-fr@gmail.com',
-    description='A package to print messages with douroucoulis ASCII art.',
+    description='A package to print messages with douroucouli ASCII art.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/douroucoulis-fr/douroucoulisay.git',
---
+++ test.py
@@ -0,0 +1,3 @@
+from douroucoulisay.douroucoulisay import douroucoulisay
+
+douroucoulisay("Douroucoulis, mirikinas, musmukis, and night/owl monkeys are all members of the genus Aotus. We are genetically and socially monogamous, and we live in small family groups where both of our parents care for their young. We are widely spread are can be found from Panama to Argentina. We are mostly nocturnal but can use the daytime to balance major metabolic challenges, like thermoregulatory costs during the cold Argentinian winters.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHave a hoot of a day!")
